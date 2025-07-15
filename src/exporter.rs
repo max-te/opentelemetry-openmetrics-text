16752ccdb950d674ab85f663db13ad8b8bb52ea0
Maximilian Teegen <max@arch-distrobox.haruka>
2025-07-15 14:05:39 +0200

    refactor(exporter): simplify buffer swapping in exporter
    Get rid of complicated Option<String>::take() construct and use
    std::mem::swap instead. DerefMut is used to get &mut String from the
    Lock guards.

--- src/exporter.rs
+++ src/exporter.rs
@@ -1,3 +1,4 @@
+use std::ops::DerefMut;
 use std::sync::Arc;
 use std::time::Duration;
 
@@ -11,9 +12,8 @@ use crate::convert::WriteOpenMetrics;
 
 #[derive(Debug, Clone)]
 pub struct OpenMetricsExporter {
-    buffer: Arc<RwLock<Option<String>>>,
-    backbuffer: Arc<Mutex<Option<String>>>,
-    // TODO: replace this with something simpler like arc-swap
+    buffer: Arc<RwLock<String>>,
+    backbuffer: Arc<Mutex<String>>,
 }
 
 impl Default for OpenMetricsExporter {
@@ -25,13 +25,13 @@ impl Default for OpenMetricsExporter {
 impl OpenMetricsExporter {
     pub fn new() -> Self {
         OpenMetricsExporter {
-            buffer: Arc::new(RwLock::new(Some(String::new()))),
-            backbuffer: Arc::new(Mutex::new(Some(String::new()))),
+            buffer: Arc::new(RwLock::new(String::new())),
+            backbuffer: Arc::new(Mutex::new(String::new())),
         }
     }
 
     pub async fn text(&self) -> String {
-        self.buffer.read().await.as_ref().unwrap().clone()
+        self.buffer.read().await.as_str().to_owned()
     }
 }
 
@@ -40,17 +40,15 @@ impl PushMetricExporter for OpenMetricsExporter {
         #[cfg(feature = "tracing")]
         tracing::debug!("Exporting metrics");
         let mut backbuffer = self.backbuffer.lock().await;
-        let mut nextbuffer = backbuffer.take().unwrap_or_default();
-        nextbuffer.clear();
+        backbuffer.clear();
         metrics
-            .write_as_openmetrics(&mut nextbuffer)
+            .write_as_openmetrics(backbuffer.deref_mut())
             .map_err(|err| {
                 OTelSdkError::InternalFailure(format!("Failed to write to buffer: {err}"))
             })?;
 
-        let mut buffer = self.buffer.write().await;
-        let oldbuffer = buffer.replace(nextbuffer);
-        *backbuffer = oldbuffer;
+        let mut frontbuffer = self.buffer.write().await;
+        std::mem::swap(frontbuffer.deref_mut(), backbuffer.deref_mut());
 
         Ok(())
     }
